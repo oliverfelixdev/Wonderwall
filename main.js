71eb9370a5506e23be5a00aa167b3d82ba44ef8e
oliverfelixdev <oliverfelix.dev@gmail.com>
2025-01-20 13:45:09 +0530

    UI Updated

--- main.js
+++ main.js
@@ -1,19 +1,40 @@
-let swiperAllLanding = () => {
+let SwiperLnNHeadMn = () => {
+    // SWIPER MAIN - SLIDE CONFIGURATION
     let swiper = new Swiper(".mySwiper", {
         spaceBetween: 30,
         effect: "fade",
-        on: { slideChange: updateNavigationButtons, }
+        slidesPerView: 1,
+        on: {
+            slideChange: function () {
+                updateNavigationButtons();
+                updateSlideNumber(this)
+                splitTextIntoSpans()
+            },
+
+            init: function () {
+                updateSlideNumber(this);
+            }
+        }
     });
 
-    function updateNavigationButtons () {
+    function updateNavigationButtons() {
         updateButtonState('.el-prev', swiper.isBeginning);
         updateButtonState('.el-next', swiper.isEnd);
     }
 
-    function updateButtonState(selector, condition){
+    function updateButtonState(selector, condition) {
         let button = document.querySelector(selector);
-        button.style.opacity = condition ? '0.3' : '1';
+        button.style.opacity = condition ? '0.5' : '1';
         button.style.pointerEvents = condition ? 'none' : 'auto';
+    }
+
+    function updateSlideNumber(swiper) {
+        const currentSlide = swiper.realIndex + 1;
+        const totalSlide = swiper.slides.length
+
+        document.querySelector('.current-slide').textContent = currentSlide;
+        document.querySelector('.total-slide').textContent = totalSlide
+
     }
 
     updateNavigationButtons();
@@ -26,4 +47,27 @@ let swiperAllLanding = () => {
         swiper.slideNext();
     });
 
-}; swiperAllLanding();
+    // MAIN TITLE ANIMATION ON EACH SLIDE
+    function splitTextIntoSpans() {
+        const titleMain = document.querySelectorAll('.title-main')
+
+        titleMain.forEach((elem) => {
+            let clutter = ""
+            let titleMainText = elem.textContent
+            let splittedText = titleMainText.split("");
+            splittedText.forEach((char) => {
+                clutter += `<span>${char}</span>`
+            })
+
+            elem.innerHTML = clutter;
+
+        });
+
+        gsap.fromTo(".title-main span",
+            { y: 75 },
+            { stagger: 0.02, y: 0, duration: .8, ease: "power4.out", })
+
+    };
+    splitTextIntoSpans();
+
+}; SwiperLnNHeadMn();
