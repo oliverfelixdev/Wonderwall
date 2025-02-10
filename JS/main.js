4eb693f89973aae1739b7f022ebc33e7dce15db4
Oliver Felix <oliverfelix.dev@gmail.com>
2025-02-10 12:27:56 +0000

    Changes: Slider Added

--- JS/main.js
+++ JS/main.js
@@ -275,20 +275,151 @@ let hoverImageReveal = () => {
 
 }; hoverImageReveal();
 
-let cnstrctrVidCntrls = document.querySelector(".constructorVideoControls");
-let cnstrctrVideo = document.querySelector(".constructor_video");
+let showVideoConfig = () => {
+    let cnstrctrVidCntrls = document.querySelector(".constructorVideoControls");
+    let cnstrctrVideo = document.querySelector(".constructor_video");
 
-cnstrctrVidCntrls.addEventListener("click", () => {
-    cnstrctrVidCntrls.style.display = "none"
-    cnstrctrVideo.play();
-})
+    cnstrctrVidCntrls.addEventListener("click", () => {
+        cnstrctrVidCntrls.style.display = "none"
+        cnstrctrVideo.play();
+    })
 
-cnstrctrVideo.addEventListener("click", () => {
-    cnstrctrVideo.pause();
-    cnstrctrVidCntrls.style.display = "flex"
-    cnstrctrVideo.load();
-})
+    cnstrctrVideo.addEventListener("click", () => {
+        cnstrctrVideo.pause();
+        cnstrctrVidCntrls.style.display = "flex"
+        cnstrctrVideo.load();
+    })
 
-cnstrctrVideo.addEventListener("contextmenu", e => {
-    e.preventDefault();
-})
+    cnstrctrVideo.addEventListener("contextmenu", e => {
+        e.preventDefault();
+    })
+
+}; showVideoConfig();
+
+let s4SldiderSwprConfig = () => {
+    var swiper = new Swiper(".s4-mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        freeMode: true,
+        grab: true
+    });
+}; s4SldiderSwprConfig();
+
+let s4TrailImageEffect = () => {
+    {
+        const container = document.querySelector(".instagram-main");
+        if (!container) return;
+
+        const MathUtils = {
+            lerp: (a, b, n) => (1 - n) * a + n * b,
+            distance: (x1, y1, x2, y2) => Math.hypot(x2 - x1, y2 - y1),
+        };
+
+        const getMousePos = (ev) => {
+            let posx = ev.clientX - container.getBoundingClientRect().left;
+            let posy = ev.clientY - container.getBoundingClientRect().top;
+            return { x: posx, y: posy };
+        };
+
+        let mousePos = { x: 0, y: 0 }, lastMousePos = { x: 0, y: 0 }, cacheMousePos = { x: 0, y: 0 };
+
+        container.addEventListener("mousemove", (ev) => (mousePos = getMousePos(ev)));
+
+        const getMouseDistance = () =>
+            MathUtils.distance(mousePos.x, mousePos.y, lastMousePos.x, lastMousePos.y);
+
+        class Image {
+            constructor(el) {
+                this.DOM = { el: el };
+                this.defaultStyle = { scale: 1, x: 0, y: 0, opacity: 0 };
+                this.getRect();
+            }
+
+            getRect() {
+                this.rect = this.DOM.el.getBoundingClientRect();
+            }
+            isActive() {
+                return gsap.getTweensOf(this.DOM.el).length > 0 || this.DOM.el.style.opacity != 0;
+            }
+        }
+
+        class ImageTrail {
+            constructor() {
+                this.DOM = { content: container.querySelector(".s4-content") };
+                this.images = [];
+                [...this.DOM.content.querySelectorAll("img")].forEach((img) =>
+                    this.images.push(new Image(img))
+                );
+                this.imagesTotal = this.images.length;
+                this.imgPosition = 0;
+                this.zIndexVal = 1;
+                this.threshold = 100;
+                requestAnimationFrame(() => this.render());
+            }
+
+            render() {
+                let distance = getMouseDistance();
+                cacheMousePos.x = MathUtils.lerp(cacheMousePos.x || mousePos.x, mousePos.x, 0.1);
+                cacheMousePos.y = MathUtils.lerp(cacheMousePos.y || mousePos.y, mousePos.y, 0.1);
+
+                if (distance > this.threshold) {
+                    this.showNextImage();
+                    ++this.zIndexVal;
+                    this.imgPosition = this.imgPosition < this.imagesTotal - 1 ? this.imgPosition + 1 : 0;
+                    lastMousePos = mousePos;
+                }
+
+                let isIdle = true;
+                for (let img of this.images) {
+                    if (img.isActive()) {
+                        isIdle = false;
+                        break;
+                    }
+                }
+                if (isIdle && this.zIndexVal !== 1) {
+                    this.zIndexVal = 1;
+                }
+                requestAnimationFrame(() => this.render());
+            }
+
+            showNextImage() {
+                const img = this.images[this.imgPosition];
+                gsap.killTweensOf(img.DOM.el);
+
+                gsap.timeline()
+                    .set(img.DOM.el, {
+                        opacity: 1,
+                        scale: 1,
+                        zIndex: this.zIndexVal,
+                        x: cacheMousePos.x - img.rect.width / 2,
+                        y: cacheMousePos.y - img.rect.height / 2
+                    })
+                    .to(img.DOM.el, { duration: 0.9, ease: "expo.out", x: mousePos.x - img.rect.width / 2, y: mousePos.y - img.rect.height / 2 })
+                    .to(img.DOM.el, { duration: 1, ease: "power1.out", opacity: 0 }, 0.4)
+                    .to(img.DOM.el, { duration: 1, ease: "quint.out", scale: 0.2 }, 0.4);
+            }
+        }
+
+        const preloadImages = () => {
+            return new Promise((resolve) => {
+                imagesLoaded(container.querySelectorAll(".s4-content__img"), resolve);
+            });
+        };
+
+        preloadImages().then(() => {
+            new ImageTrail();
+        });
+    }
+}; s4TrailImageEffect();
+
+
+// Initialize Lenis
+const lenis = new Lenis();
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
